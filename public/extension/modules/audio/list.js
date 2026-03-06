962731cd1f596622e1322024588bc0291e70ba7b
MahmoudSalah53 <mahmoudsalahmansour53@gmail.com>
2026-03-06 02:49:48 +0200

    Adding phrases (or versions) of sending blessings on the Prophet

--- public/extension/modules/audio/list.js
+++ public/extension/modules/audio/list.js
@@ -2,11 +2,21 @@ export const audios = [
     {
         id: "default",
         name: "صَلِّ عَلَىٰ مُحَمَّدٍ",
-        path: "modules/audio/audios/audio.mp3"
+        path: "modules/audio/audios/audio1.wav"
     },
     {
         id: "audio2",
-        name: "اللَّهُمَّ صَلِّ وَسَلِّمْ وَبَارِكْ عَلَىٰ سَيِّدِنَا مُحَمَّدٍ",
-        path: "modules/audio/audios/audio2.mp3"
+        name: "اللَّهُمَّ صَلِّ وَسَلِّمْ عَلَىٰ نَبِيِّنَا مُحَمَّدٍ",
+        path: "modules/audio/audios/audio2.wav"
+    },
+    {
+        id: "audio3",
+        name: "اللَّهُمَّ صَلِّ عَلَىٰ مُحَمَّدٍ النَّبِيِّ الأُمِّيِّ وَعَلَىٰ آلِهِ وَصَحْبِهِ وَسَلِّمْ",
+        path: "modules/audio/audios/audio3.wav"
+    },
+    {
+        id: "audio4",
+        name: "اللَّهُمَّ صَلِّ عَلَىٰ مُحَمَّدٍ وَأَزْوَاجِهِ وَذُرِّيَّتِهِ",
+        path: "modules/audio/audios/audio4.ogg"
     }
 ];
